42ef35a0fcaa6253453971dfa6cee84a87d80ef7
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-04-18 12:13:18 +0300

    update

--- starter-template.py
+++ starter-template.py
@@ -8,18 +8,27 @@ class Pet:
 
     def eat(self):
         # TODO
-
+        pass
     def sleep(self):
         # TODO
-
+        pass
     def play(self):
         # TODO
-
+        pass
     def train(self, trick):
         # TODO
-
+        pass
+    def show_tricks(self):
+        # TODO
+        pass
+    def get_status(self):
+        # TODO
+        pass
+        pass
+        pass
     def show_tricks(self):
         # TODO
 
     def get_status(self):
         # TODO
+     pass
